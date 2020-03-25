189cad07128e03fd5d6f90c5c9e78700bcf9ec05
Fredrik Holmqvist <tqh@users.noreply.github.com>
2020-03-25 21:05:25 +0100

    Update efi-x86_64.sh

--- efi-x86_64.sh
+++ efi-x86_64.sh
@@ -2,7 +2,7 @@
 
 qemu-system-x86_64 \
   -serial file:debug.log -global isa-debugcon.iobase=0x402 \
-	-drive if=pflash,format=raw,unit=0,readonly,file=OVMF_CODE.fd \
+  -drive if=pflash,format=raw,unit=0,readonly,file=OVMF_CODE.fd \
   -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd \
   -drive file=fat:rw:hd \
   -net none
