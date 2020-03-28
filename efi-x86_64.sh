93ab6595ab137afad0b2ca1366dd52d191b66ba0
Fredrik Holmqvist <fredrik.holmqvist@gmail.com>
2020-03-28 19:24:43 +0100

    Use machine type q35, makes OVMF boot much faster

--- efi-x86_64.sh
+++ efi-x86_64.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-qemu-system-x86_64 \
+qemu-system-x86_64 -machine q35 -vga std -net none -usb -device usb-tablet \
+	-m 128M -cpu host -enable-kvm -nodefaults -smp 1,cores=1 \
   -serial file:debug.log -global isa-debugcon.iobase=0x402 \
   -drive if=pflash,format=raw,unit=0,readonly,file=OVMF_CODE.fd \
   -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd \
-  -drive file=fat:rw:hd \
-  -net none
+  -drive file=fat:rw:hd,format=raw
